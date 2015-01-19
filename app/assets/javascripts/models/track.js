72f823c081c4fd1a0fe51e6d4d51bbce46c4d181
CJ Avilla <cjavilla@gmail.com>
2015-01-19 08:54:13 -0800

    Can type and get error feedback

--- app/assets/javascripts/models/track.js
+++ app/assets/javascripts/models/track.js
@@ -1,3 +1,59 @@
 CodeRacer.Models.Track = Backbone.Model.extend({
-  urlRoot: '/api/tracks'
+  urlRoot: '/api/tracks',
+
+  checkWord: function (word) {
+    return this.wordChecker().checkWord(word);
+  },
+
+  checkLetter: function (letter) {
+    return this.wordChecker().checkLetter(letter);
+  },
+
+  wordComplete: function (word) {
+    return this.wordChecker().wordComplete(word);
+  },
+
+  backSpace: function () {
+    return this.wordChecker().backSpace();
+  },
+
+  wordChecker: function () {
+    if(!this._wordChecker) {
+      this._wordChecker = new WordChecker(this.get('content'));
+    }
+    return this._wordChecker;
+  },
 });
+
+function WordChecker(content) {
+  this.words = this.splitIntoWords(content);
+  this.currentIndex = 0;
+}
+
+WordChecker.prototype.splitIntoWords = function(content) {
+  return content.split(" ");
+};
+
+WordChecker.prototype.currentWord = function() {
+  return this.words[this.currentIndex];
+};
+
+WordChecker.prototype.currentPaddedWord = function() {
+  return this.currentWord() + ' ';
+};
+
+WordChecker.prototype.wordComplete = function (word) {
+  if(this.currentPaddedWord() === word) {
+    this.currentIndex++;
+    return true;
+  }
+  return false;
+};
+
+WordChecker.prototype.checkWord = function(word) {
+  if(this.currentWord().startsWith(word)) {
+    return true;
+  }
+
+  return false;
+};
--- app/assets/javascripts/views/tracks/tracks_detail.js
+++ app/assets/javascripts/views/tracks/tracks_detail.js
@@ -5,6 +5,28 @@ CodeRacer.Views.TrackDetail = Backbone.View.extend({
 
   template: JST['tracks/detail'],
 
+  events: {
+    'keyup input': 'handleKeyUp'
+  },
+
+  handleKeyUp: function (event) {
+    console.log(event.keyCode);
+    if(event.keyCode === 16) {
+      return;
+    }
+    if(event.keyCode === 32) {
+      if(this.model.wordComplete(this.$('input').val())) {
+        this.$('input').val('');
+        return;
+      }
+    }
+    if(!this.model.checkWord(this.$('input').val())) {
+      this.$('input').addClass('wrong');
+    } else {
+      this.$('input').removeClass('wrong');
+    }
+  },
+
   render: function () {
     var content = this.template({
       track: this.model
