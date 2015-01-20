9ee2b8e05308dcc503304ad58ffe2a274a01b6d2
CJ Avilla <cjavilla@gmail.com>
2015-01-20 10:00:04 -0800

    adding polyfil

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,15 @@
 //= require_tree ./views
 //= require_tree ./routers
 //= require_tree .
+
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: function (searchString, position) {
+      position = position || 0;
+      return this.lastIndexOf(searchString, position) === position;
+    }
+  });
+}
