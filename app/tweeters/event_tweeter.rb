1042bc95efc2f9115daf282cbdcc89858236b673
CJ Avilla <cjavilla@gmail.com>
2015-01-27 14:15:49 -0800

    add hashtag

--- app/tweeters/event_tweeter.rb
+++ app/tweeters/event_tweeter.rb
@@ -2,14 +2,14 @@ class EventTweeter < ApplicationTweeter
   def new_leader(leader, entry)
     tweet(
       to: leader.nickname,
-      body: "Awesome! #{ entry.wpm } WPM puts you at the top of #{ entry.race.track.title } http://goo.gl/hbwfvb"
+      body: "Awesome! #{ entry.wpm } #WPM puts you at the top of #{ entry.race.track.title } http://goo.gl/hbwfvb"
     )
   end
 
   def cheater(user, entry)
     tweet(
       to: user.nickname,
-      body: ":( no way you typed #{ entry.wpm } WPM. hit me up with the code and you'll get up here: goo.gl/YwTEh8"
+      body: ":( no way you typed #{ entry.wpm } #WPM. hit me up with the code and you'll get up here: goo.gl/YwTEh8"
     )
   end
 
@@ -23,7 +23,7 @@ class EventTweeter < ApplicationTweeter
 
     tweet(
       to: old.nickname,
-      body: "Awe dang! #{ name } beat your score on #{ entry.race.track.title } /w #{ entry.wpm }. You can catch'em! http://goo.gl/hbwfvb"
+      body: "Awe dang! #{ name } beat your #WPM on #{ entry.race.track.title } /w #{ entry.wpm }. You can catch'em! http://goo.gl/hbwfvb"
     )
   end
 end
